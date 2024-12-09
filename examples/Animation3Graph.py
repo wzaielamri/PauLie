d6f5c8649eb2d622c8af4b3bf8b2c2ac4a8f9534
unknown <kgolovkin@mail.ru>
2024-12-09 23:07:13 +0300

    save animation

--- examples/Animation3Graph.py
+++ examples/Animation3Graph.py
@@ -1,5 +1,4 @@
 from PauLie import *
-from PauLie.common.algebras import *
 
 if __name__ == '__main__':
     animationAntiCommutationGraph(["XYI", "IXY", "XZY"], storage={"filename":"data/animation3Graph.gif", "writer":"pillow"})
