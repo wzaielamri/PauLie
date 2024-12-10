be2d63083a5b0f55bf93769e8700ee4bb668e81e
oxana <oxana.shaya@rwth-aachen.de>
2024-12-10 12:31:57 +0100

    type b examples

--- examples/Animation3Graph.py
+++ examples/Animation3Graph.py
@@ -1,5 +1,5 @@
 from PauLie import *
 
 if __name__ == '__main__':
-    animationAntiCommutationGraph(["XYI", "IXY", "XZY"], storage={"filename":"PauLie/docs/source/media/example.html", "writer":"html"})
-
+    animationAntiCommutationGraph(["XYI", "IXY", "XZY"], storage={"filename":"PauLie/docs/source/media/example_a.html", "writer":"html"})
+    animationAntiCommutationGraph(["XY", "XZ"], size = 4, storage={"filename": "PauLie/docs/source/media/example_b.html", "writer": "html"})
