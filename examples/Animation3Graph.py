f2c423db78383ae6682e5bcc46d6faf6c8ccaac7
oxana <oxana.shaya@rwth-aachen.de>
2024-12-10 11:16:27 +0100

    add files

--- examples/Animation3Graph.py
+++ examples/Animation3Graph.py
@@ -1,5 +1,5 @@
 from PauLie import *
 
 if __name__ == '__main__':
-    animationAntiCommutationGraph(["XYI", "IXY", "XZY"], storage={"filename":"data/animation3Graph.gif", "writer":"pillow"})
+    animationAntiCommutationGraph(["XYI", "IXY", "XZY"], storage={"filename":"PauLie/docs/source/media/example.html", "writer":"html"})
 
