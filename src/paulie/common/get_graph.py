e6c8a5967e502179a0204372c7e0e8f4ac608af6
unknown <kgolovkin@mail.ru>
2025-04-17 19:58:36 +0300

    change get_graph

--- src/paulie/common/get_graph.py
+++ src/paulie/common/get_graph.py
@@ -1,3 +1,5 @@
+from itertools import combinations
+
 def get_graph(generators, commutators=[]):
     """
     Get graph
@@ -6,16 +8,13 @@ def get_graph(generators, commutators=[]):
         commutators: The area of Pauli strings over which to build a graph. If not specified, then that's it
     Returns the vertices, edges, and labels of edges
     """
-    vertices = []
+    vertices = [str(g) for g in generators]
     edge_labels = {}
     edges = []
-    for a in generators:
-        vertices.append(str(a))
-        for b in generators:
-            if a < b:
-                if not a|b:
-                    c = a^b
-                    if len(commutators) == 0 or c in commutators:
-                        edges.append((str(a), str(b)))
-                        edge_labels[(str(a), str(b))] = str(c)
+    for a, b in combinations(generators, 2):
+        c = a^b
+        if c and (len(commutators) == 0 or c in commutators):
+            edges.append((str(a), str(b)))
+            edge_labels[(str(a), str(b))] = str(c)
     return vertices, edges, edge_labels
+
