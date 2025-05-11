8c6e506648ba1bcd6e0d9c8619f0af31b44326ee
unknown <kgolovkin@mail.ru>
2025-05-11 18:38:00 +0300

    workflow lint

--- examples/animation_graph_docs.py
+++ examples/animation_graph_docs.py
@@ -1,8 +1,12 @@
+"""
+Bulding animation
+"""
 from paulie.application.animation import animation_anti_commutation_graph
-from paulie.common.pauli_string_factory import get_pauli_string as p 
+from paulie.common.pauli_string_factory import get_pauli_string as p
 
 
 if __name__ == "__main__":
-
-    animation_anti_commutation_graph(p(["XYI", "IXY", "XZY"]), initGraph=True, storage={"filename":"data/example_a.html", "writer":"html"})
-    animation_anti_commutation_graph(p(["XY", "XZ"], n = 4), initGraph=True, storage={"filename": "data/example_b.html", "writer": "html"})
+    animation_anti_commutation_graph(p(["XYI", "IXY", "XZY"]),
+                                     storage={"filename":"data/example_a.html", "writer":"html"})
+    animation_anti_commutation_graph(p(["XY", "XZ"], n = 4),
+                                     storage={"filename": "data/example_b.html", "writer": "html"})
