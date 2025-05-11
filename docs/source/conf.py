b2ff84df48b36f17dbe3456674221fa286408ba9
unknown <kgolovkin@mail.ru>
2025-05-11 18:43:13 +0300

    workflow lint

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,6 @@
+"""
+Configuration file for the Sphinx documentation builder.
+"""
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
