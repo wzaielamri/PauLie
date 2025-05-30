cd0ffe394b6ff4f99d1810835a74265c53f0244b
Newtech66 <contact.mainakroy@gmail.com>
2025-05-30 16:13:13 +0530

    more optimizations

--- src/paulie/application/matrix_decomposition.py
+++ src/paulie/application/matrix_decomposition.py
@@ -2,10 +2,9 @@
     Fast Pauli basis matrix decomposition algorithm (arXiV:2403.11644).
 """
 import numpy as np
-from paulie.common.pauli_string_bitarray import PauliString
+from paulie.common.pauli_string_bitarray import PauliString, CODEC
 from paulie.common.pauli_string_factory import get_pauli_string, get_identity
 from collections import deque, Counter
-from concurrent.futures import ProcessPoolExecutor
 
 def matrix_decomposition(matrix: np.ndarray, tol: float=1e-8) -> dict[PauliString, complex]:
     # Shape checks
@@ -20,90 +19,39 @@ def matrix_decomposition(matrix: np.ndarray, tol: float=1e-8) -> dict[PauliStrin
         raise ValueError(f"expected square matrix with power of two \
                          dimensions but matrix dimensions are \
                          ({matrix.shape[0]}, {matrix.shape[1]})")
-    # Numpy dtype checks?
-    # General decomposition (future additions: tridiagonal/band-diagonal cases?)
     n = matrix.shape[0]
     log2n = int(n).bit_length() - 1
-    # Hardcoded parallel processing by splitting into 4 blocks
-    if log2n >= 3:
-        res = Counter()
-        ops = [get_pauli_string('I'),
-               get_pauli_string('X'),
-               get_pauli_string('Y'),
-               get_pauli_string('Z'),]
-        Ms = [matrix[0 : n // 2, 0 : n // 2],   # -> 0.5 * (I + Z)
-              matrix[0 : n // 2, n // 2 : n],   # -> 0.5 * (X + iY)
-              matrix[n // 2 : n, 0 : n // 2],   # -> 0.5 * (X - iY)
-              matrix[n // 2 : n, n // 2 : n],]  # -> 0.5 * (I - Z)
-        # Break up matrix
-        with ProcessPoolExecutor() as executor:
-            for i, decomp in enumerate(executor.map(_decomp_general, Ms, [tol] * 4)):
-                for pstr, coeff in decomp.items():
-                    if i == 0:
-                        res[ops[0] + pstr] += coeff / 2
-                        res[ops[3] + pstr] += coeff / 2
-                    elif i == 1:
-                        res[ops[1] + pstr] += coeff / 2
-                        res[ops[2] + pstr] += 1j * coeff / 2
-                    elif i == 2:
-                        res[ops[1] + pstr] += coeff / 2
-                        res[ops[2] + pstr] -= 1j * coeff / 2
-                    elif i == 3:
-                        res[ops[0] + pstr] += coeff / 2
-                        res[ops[3] + pstr] -= coeff / 2
-        return res
-    return dict(_decomp_general(matrix, tol))
-
-def _update_general(k: np.ndarray, m: np.ndarray, l: int, node: str) -> None:
-    # The nature of the updates forces us to walk the tree in IXYZ order
-    if node == 'I':
-        k[2 ** l : 2 ** (l + 1)] = k[0 : 2 ** l] + 2 ** l
-        m[2 ** l : 2 ** (l + 1)] = m[0 : 2 ** l]
-    elif node == 'X':
-        k[2 ** l : 2 ** (l + 1)] -= 2 ** (l + 1)
-    elif node == 'Y':
-        m[2 ** l : 2 ** (l + 1)] *= -1
-    elif node == 'Z':
-        k[2 ** l : 2 ** (l + 1)] += 2 ** (l + 1)
-
-def _decomp_general(matrix: np.ndarray, tol: float) -> dict[PauliString, complex]:
-    n = matrix.shape[0]
-    log2n = int(n).bit_length() - 1
-    k = np.zeros(n, dtype=np.int64)
-    m = np.zeros(n, dtype=np.int64)
     j = np.arange(n)
-    nY = 0
-    coeffs = Counter()
-    # Initialize m[0] (k[0] is computed at leaves)
+    k = np.zeros(n, dtype=np.int64)
+    m = np.zeros(n, dtype=np.int8)
     m[0] = 1
-    # Walk the tree
-    dfs_stack = deque([('I', 0), ('X', 0), ('Y', 0), ('Z', 0)])
-    # pstr = get_identity(log2n)
-    pstr = ['I'] * log2n
-    curlen = 0
+    coeffs = Counter()
+    dfs_stack = deque([('Z', 0, 1), ('Y', 0, -1j), ('X', 0, 1), ('I', 0, 1)])
+    pstr = get_identity(log2n)
     while dfs_stack:
-        node, depth = dfs_stack.popleft()
-        while curlen > depth:
-            rnode = pstr[log2n - curlen]
-            pstr[log2n - curlen] = 'I'
-            nY -= (rnode == 'Y')
-            curlen -= 1
-        nY += (node == 'Y')
-        pstr[log2n - curlen - 1] = node
-        curlen += 1
-        _update_general(k, m, depth, node)
-        if curlen == log2n:
-            pcl = get_pauli_string(''.join(pstr))
-            k0 = int(pcl.bits_even.to01(), base=2)
-            coeff = (-1j) ** (nY % 4) * \
-                np.dot(m, matrix[k0 + k, j]) / n
+        node, depth, phase = dfs_stack.pop()
+        if node == 'I':
+            k[2 ** depth : 2 ** (depth + 1)] = k[0 : 2 ** depth] + 2 ** depth
+            m[2 ** depth : 2 ** (depth + 1)] = m[0 : 2 ** depth]
+        elif node == 'X':
+            k[2 ** depth : 2 ** (depth + 1)] -= 2 ** (depth + 1)
+        elif node == 'Y':
+            m[2 ** depth : 2 ** (depth + 1)] *= -1
+        elif node == 'Z':
+            k[2 ** depth : 2 ** (depth + 1)] += 2 ** (depth + 1)
+        # Set bits manually for performance
+        nodebits = CODEC[node]
+        pstr.bits[2 * (log2n - depth - 1)] = nodebits[0]
+        pstr.bits[2 * (log2n - depth - 1) + 1] = nodebits[1]
+        pstr.bits_even[log2n - depth - 1] = nodebits[0]
+        pstr.bits_odd[log2n - depth - 1] = nodebits[1]
+        if depth + 1 == log2n:
+            k0 = int(pstr.bits_even.to01(), base=2)
+            coeff = np.dot(m, matrix[k0 + k, j]) / n
             if np.abs(coeff) > tol:
-                coeffs[pcl] += coeff
+                coeffs[pstr.copy()] += phase * coeff
         else:
-            dfs_stack.appendleft(('Z', 1 + depth))
-            dfs_stack.appendleft(('Y', 1 + depth))
-            dfs_stack.appendleft(('X', 1 + depth))
-            dfs_stack.appendleft(('I', 1 + depth))
+            dfs_stack.extend([('Z', depth + 1, phase), ('Y', depth + 1, -1j * phase), ('X', depth + 1, phase), ('I', depth + 1, phase)])
     return dict(coeffs)
 
 def matrix_decomposition_diagonal(diag: np.ndarray, tol: float=1e-8) -> dict[PauliString, complex]:
@@ -113,40 +61,29 @@ def matrix_decomposition_diagonal(diag: np.ndarray, tol: float=1e-8) -> dict[Pau
     if int(diag.shape[0]).bit_count() != 1:
         raise ValueError(f"expected 1D ndarray with power of two \
                          length but length is {diag.shape[0]}")
-    return dict(_decomp_diagonal(diag))
-
-def _update_diagonal(m: np.ndarray, l: int, node: str) -> None:
-    if node == 'I':
-        m[2 ** l : 2 ** (l + 1)] = m[0 : 2 ** l]
-    elif node == 'Z':
-        m[2 ** l : 2 ** (l + 1)] *= -1
-
-def _decomp_diagonal(diag: np.ndarray, tol: float) -> dict[PauliString, complex]:
     n = diag.shape[0]
     log2n = int(n).bit_length() - 1
-    m = np.zeros(n, dtype=np.int64)
-    coeffs = Counter()
+    m = np.zeros(n, dtype=np.int8)
     m[0] = 1
-    # Walk the tree
-    dfs_stack = deque([('I', 0), ('Z', 0)])
+    coeffs = Counter()
+    dfs_stack = deque([('Z', 0), ('I', 0)])
     pstr = get_identity(log2n)
-    curlen = 0
-    pos = log2n - 1
     while dfs_stack:
-        node, depth = dfs_stack.popleft()
-        while curlen > depth:
-            pstr[pos + 1] = 'I'
-            curlen -= 1
-            pos += 1
-        pstr[pos] = node
-        curlen += 1
-        pos -= 1
-        _update_diagonal(m, depth, node)
-        if curlen == log2n:
+        node, depth = dfs_stack.pop()
+        if node == 'I':
+            m[2 ** depth : 2 ** (depth + 1)] = m[0 : 2 ** depth]
+        elif node == 'Z':
+            m[2 ** depth : 2 ** (depth + 1)] *= -1
+        # Set bits manually for performance
+        nodebits = CODEC[node]
+        pstr.bits[2 * (log2n - depth - 1)] = nodebits[0]
+        pstr.bits[2 * (log2n - depth - 1) + 1] = nodebits[1]
+        pstr.bits_even[log2n - depth - 1] = nodebits[0]
+        pstr.bits_odd[log2n - depth - 1] = nodebits[1]
+        if depth + 1 == log2n:
             coeff = np.dot(m, diag) / n
             if np.abs(coeff) > tol:
                 coeffs[pstr.copy()] += coeff
         else:
-            dfs_stack.appendleft(('Z', 1 + depth))
-            dfs_stack.appendleft(('I', 1 + depth))
+            dfs_stack.extend([('Z', depth + 1), ('I', depth + 1)])
     return dict(coeffs)
