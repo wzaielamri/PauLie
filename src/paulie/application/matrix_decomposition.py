62c9d75391d72a6069fbb395aa436fcbcb9a035f
Newtech66 <contact.mainakroy@gmail.com>
2025-05-31 21:46:48 +0530

    Added Fast Pauli Transform

--- src/paulie/application/matrix_decomposition.py
+++ src/paulie/application/matrix_decomposition.py
@@ -3,11 +3,31 @@
 """
 import numpy as np
 from paulie.common.pauli_string_bitarray import PauliString, CODEC
-from paulie.common.pauli_string_factory import get_pauli_string, get_identity
+from paulie.common.pauli_string_factory import get_identity
 from collections import deque, Counter
 
-def matrix_decomposition(matrix: np.ndarray, tol: float=1e-8) -> dict[PauliString, complex]:
-    # Shape checks
+def _mat_to_vec(A: np.ndarray) -> list[complex]:
+    if A.shape[0] == 2:
+        return [A[0, 0], A[1, 1], A[0, 1], A[1, 0]]
+    n = A.shape[0] // 2
+    return _mat_to_vec(A[:n,:n]) + _mat_to_vec(A[n:,n:]) + _mat_to_vec(A[:n,n:]) + _mat_to_vec(A[n:,:n])
+
+def _fast_pauli_transform(a: list) -> None:
+    h = 1
+    while h < len(a):
+        for i in range(0, len(a), h << 2):
+            for j in range(i, i + h):
+                x = a[j]
+                y = a[j + h]
+                z = a[j + 2 * h]
+                w = a[j + 3 * h]
+                a[j] = (x + y) / 2
+                a[j + h] = (x - y) / 2
+                a[j + 2 * h] = (z + w) / 2
+                a[j + 3 * h] = 1j * (z - w) / 2
+        h <<= 2
+
+def matrix_decomposition(matrix: np.ndarray, tol: float=1e-8) -> dict[PauliString, np.complex128]:
     if matrix.ndim != 2:
         raise ValueError("matrix must be a 2D ndarray")
     if matrix.shape[0] != matrix.shape[1]:
@@ -21,41 +41,15 @@ def matrix_decomposition(matrix: np.ndarray, tol: float=1e-8) -> dict[PauliStrin
                          ({matrix.shape[0]}, {matrix.shape[1]})")
     n = matrix.shape[0]
     log2n = int(n).bit_length() - 1
-    j = np.arange(n)
-    k = np.zeros(n, dtype=np.int64)
-    m = np.zeros(n, dtype=np.int8)
-    k0 = 0
-    m[0] = 1
-    coeffs = Counter()
-    dfs_stack = deque([('Z', 0), ('Y', 0), ('X', 0), ('I', 0)])
-    phase = [1, -1j, -1, 1j]
+    B = _mat_to_vec(matrix)
+    _fast_pauli_transform(B)
     pstr = get_identity(log2n)
-    while dfs_stack:
-        node, depth = dfs_stack.pop()
-        if node == 'I':
-            k[2 ** depth : 2 ** (depth + 1)] = k[0 : 2 ** depth] + 2 ** depth
-            m[2 ** depth : 2 ** (depth + 1)] = m[0 : 2 ** depth]
-        elif node == 'X':
-            k0 += 2 ** depth
-            k[2 ** depth : 2 ** (depth + 1)] -= 2 ** (depth + 1)
-        elif node == 'Y':
-            m[2 ** depth : 2 ** (depth + 1)] *= -1
-        elif node == 'Z':
-            k0 -= 2 ** depth
-            k[2 ** depth : 2 ** (depth + 1)] += 2 ** (depth + 1)
-        # Set bits manually for performance
-        nodebits = CODEC[node]
-        pstr.bits[2 * (log2n - depth - 1)] = nodebits[0]
-        pstr.bits[2 * (log2n - depth - 1) + 1] = nodebits[1]
-        pstr.bits_even[log2n - depth - 1] = nodebits[0]
-        pstr.bits_odd[log2n - depth - 1] = nodebits[1]
-        if depth + 1 == log2n:
-            coeff = np.dot(m, matrix[k0 + k, j]) / n
-            if np.abs(coeff) > tol:
-                coeffs[pstr.copy()] += phase[(pstr.bits_even & pstr.bits_odd).count() % 4] * coeff
-        else:
-            dfs_stack.extend([('Z', depth + 1), ('Y', depth + 1), ('X', depth + 1), ('I', depth + 1)])
-    return dict(coeffs)
+    res = dict()
+    for i in range(4 ** log2n):
+        if np.abs(B[i]) > tol:
+            res[pstr.copy()] = B[i]
+        pstr.inc()
+    return res
 
 def matrix_decomposition_diagonal(diag: np.ndarray, tol: float=1e-8) -> dict[PauliString, complex]:
     # Shape checks
