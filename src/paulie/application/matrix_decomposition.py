315bfd4f7f46fa047e2123862db3c04280b861b8
unknown <contact.mainakroy@gmail.com>
2025-06-04 23:18:15 +0530

    modified matrix decomposition functions to return only weight vector

--- src/paulie/application/matrix_decomposition.py
+++ src/paulie/application/matrix_decomposition.py
@@ -2,9 +2,6 @@
     Fast Pauli basis matrix decomposition algorithm.
 """
 import numpy as np
-from paulie.common.pauli_string_bitarray import PauliString
-from paulie.common.pauli_string_factory import get_identity
-from bitarray import bitarray
 
 def _pauli_ord(row: np.ndarray, col: np.ndarray, n: int) -> None:
     if n == 1:
@@ -29,16 +26,7 @@ def _mat_to_vec(A: np.ndarray, log2n: int) -> np.ndarray:
     flatindex = (1 << log2n) * row + col
     return A.reshape(-1)[flatindex].astype(np.complex128)
 
-def _one_bit_indices(x: np.int64, n: int) -> list:
-    x = int(x)
-    one_bit_indexes = []
-    while x:
-        v = x & -x
-        one_bit_indexes.append(n - v.bit_length())
-        x ^= v
-    return one_bit_indexes
-
-def matrix_decomposition(matrix: np.ndarray, tol: float=1e-8) -> dict[PauliString, np.complex128]:
+def matrix_decomposition(matrix: np.ndarray, tol: float=1e-8) -> np.ndarray:
     if matrix.ndim != 2:
         raise ValueError("matrix must be a 2D ndarray")
     if matrix.shape[0] != matrix.shape[1]:
@@ -59,27 +47,9 @@ def matrix_decomposition(matrix: np.ndarray, tol: float=1e-8) -> dict[PauliStrin
             B[i : i + h], B[i + h : i + 2 * h] = (B[i : i + h] + B[i + h : i + 2 * h]) / 2, (B[i : i + h] - B[i + h : i + 2 * h]) / 2
             B[i + 2 * h : i + 3 * h], B[i + 3 * h : i + 4 * h] = (B[i + 2 * h : i + 3 * h] + B[i + 3 * h : i + 4 * h]) / 2, 1j * (B[i + 2 * h : i + 3 * h] - B[i + 3 * h : i + 4 * h]) / 2
         h *= 4
-    pstr = get_identity(log2n)
-    res = dict()
-    # Find nonzero coefficient indices
-    nnz_coeff_inds = np.where(np.abs(B) > tol)[0]
-    if nnz_coeff_inds[0] == 0:
-        res[pstr.copy()] = B[0]
-        nnz_coeff_inds = nnz_coeff_inds[1:]
-    npb = np.zeros(2 * log2n, dtype=np.bool)
-    j = 0
-    for i in nnz_coeff_inds:
-        # Manually set bits for performance
-        npb[_one_bit_indices(i ^ j, 2 * log2n)] ^= True
-        pstr.bits = bitarray()
-        pstr.bits.pack(npb.tobytes())
-        pstr.bits_even = pstr.bits[::2]
-        pstr.bits_odd = pstr.bits[1::2]
-        res[pstr.copy()] = B[i]
-        j = i
-    return res
+    return B
 
-def matrix_decomposition_diagonal(diag: np.ndarray, tol: float=1e-8) -> dict[PauliString, np.complex128]:
+def matrix_decomposition_diagonal(diag: np.ndarray, tol: float=1e-8) -> np.ndarray:
     if diag.ndim != 1:
         raise ValueError("matrix must be a 1D ndarray")
     if diag.shape[0] == 1:
@@ -95,21 +65,4 @@ def matrix_decomposition_diagonal(diag: np.ndarray, tol: float=1e-8) -> dict[Pau
         for i in range(0, B.shape[0], 2 * h):
             B[i : i + h], B[i + h : i + 2 * h] = (B[i : i + h] + B[i + h : i + 2 * h]) / 2, (B[i : i + h] - B[i + h : i + 2 * h]) / 2
         h *= 2
-    pstr = get_identity(log2n)
-    res = dict()
-    # Find nonzero coefficient indices
-    nnz_coeff_inds = np.where(np.abs(B) > tol)[0]
-    if nnz_coeff_inds[0] == 0:
-        res[pstr.copy()] = B[0]
-        nnz_coeff_inds = nnz_coeff_inds[1:]
-    npb = np.zeros(log2n, dtype=np.bool)
-    j = 0
-    for i in nnz_coeff_inds:
-        # Manually set bits for performance
-        npb[_one_bit_indices(i ^ j, log2n)] ^= True
-        pstr.bits_odd = bitarray()
-        pstr.bits_odd.pack(npb.tobytes())
-        pstr.bits[1::2] = pstr.bits_odd
-        res[pstr.copy()] = B[i]
-        j = i
-    return res
+    return B
