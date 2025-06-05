2037b5dbac45414c9c50137489cd95b91f761351
unknown <contact.mainakroy@gmail.com>
2025-06-05 11:14:56 +0530

    Add docstrings and other linting issues

--- src/paulie/application/matrix_decomposition.py
+++ src/paulie/application/matrix_decomposition.py
@@ -19,50 +19,72 @@ def _pauli_ord(row: np.ndarray, col: np.ndarray, n: int) -> None:
     row[3 * pw: 4 * pw] = row[:pw] + (1 << (n - 1))
     col[3 * pw: 4 * pw] = col[:pw]
 
-def _mat_to_vec(A: np.ndarray, log2n: int) -> np.ndarray:
+def _mat_to_vec(matrix: np.ndarray) -> np.ndarray:
+    """
+    Vectorizes a matrix in Pauli order.
+                                                          [vec(A)]
+    Given a matrix M = [A B], we vectorize it as vec(M) = [vec(D)].
+                       [C D]                              [vec(B)]
+                                                          [vec(C)]
+
+    Args:
+        matrix: The matrix to vectorize.
+    """
+    log2n = int(matrix.shape[0]).bit_length() - 1
     row = np.zeros(4 ** log2n, dtype=np.int64)
     col = np.zeros(4 ** log2n, dtype=np.int64)
     _pauli_ord(row, col, log2n)
-    flatindex = (1 << log2n) * row + col
-    return A.reshape(-1)[flatindex].astype(np.complex128)
+    flat_index = (1 << log2n) * row + col
+    return matrix.reshape(-1)[flat_index].astype(np.complex128)
 
-def matrix_decomposition(matrix: np.ndarray, tol: float=1e-8) -> np.ndarray:
+def matrix_decomposition(matrix: np.ndarray) -> np.ndarray:
+    """
+    Return the weight vector corresponding to the Pauli basis decomposition of a matrix.
+
+    Args:
+        matrix: The matrix to be decomposed.
+    """
     if matrix.ndim != 2:
         raise ValueError("matrix must be a 2D ndarray")
     if matrix.shape[0] != matrix.shape[1]:
         raise ValueError(f"expected square matrix but matrix dimensions \
                          are ({matrix.shape[0]}, {matrix.shape[1]})")
     if matrix.shape[0] == 1:
-        raise ValueError(f"input must be a matrix, not a scalar")
+        raise ValueError("input must be a matrix, not a scalar")
     if int(matrix.shape[0]).bit_count() != 1:
         raise ValueError(f"expected square matrix with power of two \
                          dimensions but matrix dimensions are \
                          ({matrix.shape[0]}, {matrix.shape[1]})")
-    n = matrix.shape[0]
-    log2n = int(n).bit_length() - 1
-    B = _mat_to_vec(matrix, log2n)
+    b = _mat_to_vec(matrix)
     h = 1
-    while h < B.shape[0]:
-        for i in range(0, B.shape[0], 4 * h):
-            B[i : i + h], B[i + h : i + 2 * h] = (B[i : i + h] + B[i + h : i + 2 * h]) / 2, (B[i : i + h] - B[i + h : i + 2 * h]) / 2
-            B[i + 2 * h : i + 3 * h], B[i + 3 * h : i + 4 * h] = (B[i + 2 * h : i + 3 * h] + B[i + 3 * h : i + 4 * h]) / 2, 1j * (B[i + 2 * h : i + 3 * h] - B[i + 3 * h : i + 4 * h]) / 2
+    while h < b.shape[0]:
+        for i in range(0, b.shape[0], 4 * h):
+            x, y = b[i : i + h], b[i + h : i + 2 * h]
+            b[i : i + h], b[i + h : i + 2 * h] = (x + y) / 2, (x - y) / 2
+            z, w = b[i + 2 * h : i + 3 * h], b[i + 3 * h : i + 4 * h]
+            b[i + 2 * h : i + 3 * h], b[i + 3 * h : i + 4 * h] = (z + w) / 2, 1j * (z - w) / 2
         h *= 4
-    return B
+    return b
 
-def matrix_decomposition_diagonal(diag: np.ndarray, tol: float=1e-8) -> np.ndarray:
+def matrix_decomposition_diagonal(diag: np.ndarray) -> np.ndarray:
+    """
+    Return the weight vector corresponding to the Pauli basis decomposition of a diagonal matrix.
+
+    Args:
+        diag: The main diagonal of the diagonal matrix to be decomposed.
+    """
     if diag.ndim != 1:
         raise ValueError("matrix must be a 1D ndarray")
     if diag.shape[0] == 1:
-        raise ValueError(f"input cannot be scalar")
+        raise ValueError("input cannot be scalar")
     if int(diag.shape[0]).bit_count() != 1:
         raise ValueError(f"expected 1D ndarray with power of two \
                          length but length is {diag.shape[0]}")
-    n = diag.shape[0]
-    log2n = int(n).bit_length() - 1
-    B = diag.astype(np.complex128)
+    b = diag.astype(np.complex128)
     h = 1
-    while h < B.shape[0]:
-        for i in range(0, B.shape[0], 2 * h):
-            B[i : i + h], B[i + h : i + 2 * h] = (B[i : i + h] + B[i + h : i + 2 * h]) / 2, (B[i : i + h] - B[i + h : i + 2 * h]) / 2
+    while h < b.shape[0]:
+        for i in range(0, b.shape[0], 2 * h):
+            x, y = b[i : i + h], b[i + h : i + 2 * h]
+            b[i : i + h], b[i + h : i + 2 * h] = (x + y) / 2, (x - y) / 2
         h *= 2
-    return B
+    return b
