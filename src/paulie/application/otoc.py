9ae9ac289a0a9879935b64dd1085adda21f35133
unknown <contact.mainakroy@gmail.com>
2025-06-09 20:42:08 +0530

    Use BFS to compute OTOC

--- src/paulie/application/otoc.py
+++ src/paulie/application/otoc.py
@@ -1,7 +1,7 @@
 """
     Compute the average out-of-time-order correlator between two Pauli strings.
 """
-import networkx as nx
+from collections import deque
 from paulie.common.pauli_string_factory import get_pauli_string as p
 from paulie.common.pauli_string_collection import PauliStringCollection
 from paulie.common.pauli_string_bitarray import PauliString
@@ -23,14 +23,20 @@ def average_otoc(generators: PauliStringCollection,
         v: Pauli string V
         w: Pauli string W
     """
-    # Generate commutator graph
-    vertices, edges = generators.get_commutator_graph()
-    graph = nx.Graph()
-    graph.add_nodes_from(vertices)
-    graph.add_edges_from(edges)
-    # Get connected component of V
-    v_connected_component = nx.node_connected_component(graph, str(v))
-    # Count the number of elements t in the connected component of V
-    # that anticommute with W
-    anticommute_count = sum(not w | p(t) for t in v_connected_component)
-    return 1 - 2 * anticommute_count / len(v_connected_component)
+    # Number of elements in connected component of V that anti-commute with W
+    anti_commute_count = 0
+    # Run BFS algorithm to discover connected component of V
+    v_connected_component_size = 0
+    visited = set()
+    queue = deque([v])
+    while queue:
+        t = queue.popleft()
+        visited.add(t)
+        if not w | t:
+            anti_commute_count += 1
+        v_connected_component_size += 1
+        for g in generators:
+            comm = t ^ g
+            if comm and comm not in visited:
+                queue.append(comm)
+    return 1 - 2 * anti_commute_count / v_connected_component_size
