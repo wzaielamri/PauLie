0f904e6c5961c487ab750fa206add6c1f8dc17c4
AmanieOxana <oxana.shaya@rwth-aachen.de>
2025-06-25 09:06:26 +0200

    test uncomment otoc

--- tests/test_otoc.py
+++ tests/test_otoc.py
@@ -92,19 +92,8 @@ def test_average_eq_initial_otoc_for_commutants(generators: list[str]) -> None:
             wmat = w.get_matrix()
             analytical_value = np.trace(wmat @ vmat @ wmat @ vmat) / d
             assert average_otoc(g, v, w) == pytest.approx(analytical_value)
+
 """ 
-@pytest.mark.parametrize("generators", generators_list)
-def test_operator_average_otoc(generators: list[str]) -> None:
-    g = p(generators)
-    i = get_identity(len(generators[0]))
-    d = 2 ** len(generators[0])
-    all_paulis = i.get_commutants()
-    operator_av = 0
-    for v,w in zip(all_paulis, all_paulis):
-            operator_av += np.abs(average_otoc(g, v, w))**2
-    assert operator_av == d * g.get_frame_potential()
-"""
-"""
 def test_su_otoc():
     for n in range(4, 12):
         g_su = p(["XX", "XY", "YZ"], n=n)
